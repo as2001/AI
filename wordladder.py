136a742ca804f84c9ce3d619681465d2e45456cb
Andrew Shao <ashao1@stuy.edu>
2018-10-25 15:35:36 -0400

    :(

--- wordladder.py
+++ wordladder.py
@@ -27,17 +27,29 @@ for x in words:
 def checkdiff(str1,str2):
     counter = 0
     for i in range(len(str1)):
-        if str1[i,i+1] == str2[i,i+1]:
+        if str1[i:i+1] == str2[i:i+1]:
             counter += 1
     return counter
 
+wr = []
+
 for x in inputs:
-    heap = [(checkdiff(x[0],x[1]),x[0],[])]
-    while heap[-1][1] != x[1]:
-        word = d[heapq.heappop(heap)]
+    heap = [(checkdiff(x[0],x[1]),x[0],[x[0]])]
+    while True:
+        word = heapq.heappop(heap)
+        if word[1] == x[1]:
+            break
         for neighbor in d[word[1]]:
-            heapq.heappush((checkdiff(neighbor,x[1]) + len(word[2]) + 1,neighbor,word[2].append(word[1])))
+            new2 = word[2]
+            new2.append(neighbor)
+            word2 = (checkdiff(neighbor,x[1]) + len(word[2]),neighbor,new2)
+            print(word2)
+            heapq.heappush(heap,word2)
+    wr.append(word[2].join(','))
 
+outp.write(wr.join('\n'))
 
-        
+f.close()
+inp.close()
+outp.close()
 
